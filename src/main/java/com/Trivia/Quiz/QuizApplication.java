f501086e0d317eb96296e2dc6df5387d59730d7e
Gold <sas5198sas@gmail.com>
2022-07-10 02:54:35 -0700

    Making Application Running fine

--- src/main/java/com/Trivia/Quiz/QuizApplication.java
+++ src/main/java/com/Trivia/Quiz/QuizApplication.java
@@ -3,12 +3,12 @@ package com.Trivia.Quiz;
 import com.Trivia.Quiz.Controller.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QuizApplication {
+public class QuizApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Controller.class);
 		SpringApplication.run(QuizApplication.class, args);
 
 		System.out.println("Akash");
