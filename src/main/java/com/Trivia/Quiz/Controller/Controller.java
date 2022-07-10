bbdbf7b869c74a832e3f37b28b0e9fe61e20fba9
Gold <sas5198sas@gmail.com>
2022-07-10 04:53:30 -0700

    Adding UI to the application

--- src/main/java/com/Trivia/Quiz/Controller/Controller.java
+++ src/main/java/com/Trivia/Quiz/Controller/Controller.java
@@ -2,6 +2,7 @@ package com.Trivia.Quiz.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -11,5 +12,16 @@ public class Controller {
     public String test()
     {
         return "Hello World";
+    }
+
+    @GetMapping("/")
+    public String HomePage()
+    {
+        return "home";
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
     }
 }
