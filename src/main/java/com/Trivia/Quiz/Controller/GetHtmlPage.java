91f72318661cbf766597920de90ca440ee0d696a
Gold <sas5198sas@gmail.com>
2022-07-10 17:54:30 -0700

    Making UI work

---
+++ src/main/java/com/Trivia/Quiz/Controller/GetHtmlPage.java
@@ -0,0 +1,26 @@
+package com.Trivia.Quiz.Controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetHtmlPage {
+    public String getHtmlPage(String path)
+    {
+
+        Map<String, String> htmlPage = new HashMap<>();
+        htmlPage.put("java","java");
+        htmlPage.put("home","home");
+        htmlPage.put(null,"home");
+        htmlPage.put("index","index");
+        htmlPage.put("cpp","cpp");
+        htmlPage.put("generic","generic");
+        htmlPage.put("test","<h1>Hello World</h1>");
+        if(htmlPage.containsKey(path))
+            return htmlPage.get(path);
+
+        else
+            return "notFound";
+    }
+}
